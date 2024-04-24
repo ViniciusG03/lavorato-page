637b1d57be5f01537959931b90344693ad623f75
Vincius <viniciusgomes.33mes@gmail.com>
2024-04-24 17:27:47 -0300

    feat: update ecosystem

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const modalCadastrar = document.querySelector('.modal');
-const modalAtualizar = document.querySelector('.modal-atualizar');
+const modalCadastrar = document.querySelector(".modal");
+const modalAtualizar = document.querySelector(".modal-atualizar");
 
 const switchModalCadastrar = () => {
-    modalCadastrar.style.display = modalCadastrar.style.display === 'block' ? 'none' : 'block';
+  modalCadastrar.style.display =
+    modalCadastrar.style.display === "block" ? "none" : "block";
 };
 
 const switchModalAtualizar = () => {
-    modalAtualizar.style.display = modalAtualizar.style.display === 'block' ? 'none' : 'block';
+  modalAtualizar.style.display =
+    modalAtualizar.style.display === "block" ? "none" : "block";
 };
 
-const btnCadastrar = document.querySelector('#btn-cadastrar');
-btnCadastrar.addEventListener('click', switchModalCadastrar);
+const btnCadastrar = document.querySelector("#btn-cadastrar");
+btnCadastrar.addEventListener("click", switchModalCadastrar);
 
-const btnAtualizar = document.querySelector('#btn-atualizar');
-btnAtualizar.addEventListener('click', switchModalAtualizar);
+const btnAtualizar = document.querySelector("#btn-atualizar");
+btnAtualizar.addEventListener("click", switchModalAtualizar);
 
-window.addEventListener('click', (event) => {
-    if (event.target === modalCadastrar) {
-        switchModalCadastrar();
-    }
+window.addEventListener("click", (event) => {
+  if (event.target === modalCadastrar) {
+    switchModalCadastrar();
+  }
 
-    if (event.target === modalAtualizar) {
-        switchModalAtualizar();
-    }
+  if (event.target === modalAtualizar) {
+    switchModalAtualizar();
+  }
 });
