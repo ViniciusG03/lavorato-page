8ebd8bb8ec22a41375b57bf627c5a11b24510e3c
Vincius <viniciusgomes.33mes@gmail.com>
2024-08-26 19:26:21 -0300

    update admin

--- www/admin.php
+++ www/admin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once 'database/database.php';
 
@@ -6,24 +6,38 @@ $database = new Database();
 $db = $database->connect();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['inserir'])) {
-    
+
     // Coleta os pacientes com status diferente de "Saiu" e "Cancelado"
-    $sql = 'SELECT * FROM usuarios_teste WHERE paciente_status != "Saiu" AND paciente_status != "Cancelado" AND paciente_saida != ""';
+    $sql = "SELECT * 
+            FROM usuarios_teste
+            WHERE paciente_status != 'Saiu'
+            AND paciente_status != 'Cancelado' 
+            AND paciente_convenio != 'CASSI' 
+            AND paciente_convenio != 'UNIVIDA'
+            AND paciente_saida IS NULL
+            AND id IN (
+                SELECT MAX(id)
+                FROM usuarios_teste
+                WHERE paciente_status != 'Saiu'
+                AND paciente_status != 'Cancelado'
+                AND paciente_convenio != 'CASSI' 
+                AND paciente_convenio != 'UNIVIDA'
+                AND paciente_saida IS NULL
+                GROUP BY paciente_nome -- Ajuste conforme necessário
+            );
+            ";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
 
     // Prepara a inserção dos pacientes com os campos especificados nulos e atualizando "paciente_mes"
-    $insert_sql = 'INSERT INTO usuarios_teste (paciente_nome, paciente_convenio, paciente_status, paciente_guia,
-                paciente_especialidade, paciente_mes, paciente_section, paciente_entrada, paciente_saida,
-                paciente_valor, paciente_data_remessa, paciente_validade, paciente_lote, paciente_faturado)
-                VALUES (:paciente_nome, :paciente_convenio, NULL, NULL, :paciente_especialidade, :paciente_mes,
-                 :paciente_section, :paciente_entrada, NULL, NULL, NULL, NULL, NULL, NULL, NULL)';
-
+    $insert_sql = 'INSERT INTO usuarios_teste (paciente_nome, paciente_convenio, paciente_especialidade, 
+                paciente_mes, paciente_section, paciente_entrada)
+                VALUES (:paciente_nome, :paciente_convenio, :paciente_especialidade, :paciente_mes,
+                 :paciente_section, :paciente_entrada)';
     $insert_stmt = $db->prepare($insert_sql);
 
-    // Itera sobre os resultados e insere os novos registros com os campos atualizados
     foreach ($result as $row) {
         $insert_stmt->execute([
             ':paciente_nome' => $row['paciente_nome'],
@@ -34,6 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['inserir'])) {
             ':paciente_mes' => 'Setembro'
         ]);
     }
+
 
     $insert_stmt->closeCursor();
 }
