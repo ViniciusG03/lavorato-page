b9a624cd086902236bed4687a717381eba0c7dae
Vincius <viniciusgomes.33mes@gmail.com>
2024-08-26 18:21:30 -0300

    update admin

--- www/admin.php
+++ www/admin.php
@@ -7,7 +7,6 @@ $db = $database->connect();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['inserir'])) {
     echo '<p>Botão clicado com sucesso!</p>';
-    echo '<p>Teste</p>';
 }
 
 ?>
